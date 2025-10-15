4bdd784412b767279a0c575e9a65eee36bf3eeef
erckezha97-commits <erckezha_97@mail.ru>
2025-10-15 13:10:47 +0500

    Add bot code

---
+++ main.py
@@ -0,0 +1,19 @@
+﻿from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import os
+
+API_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я бот и я работаю!")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.reply(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
